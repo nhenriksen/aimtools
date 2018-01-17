efb1262ff35cbd73d77fa88267bf6af2a5cba7c4
nhenriksen <shireham@gmail.com>
2018-01-17 14:28:07 -0800

    tests Travis CI

--- devtools/travis/runtest.sh
+++ devtools/travis/runtest.sh
@@ -4,16 +4,4 @@ set -ex
 echo "Simplest possible testing regimen"
 cd aimtools/tests
 
-#source activate myenv
-
-#echo 'AFTER'
-which python
-python --version
-which pytest
-pytest --version
-which parmed
-parmed --version
-
 pytest test_unique_types.py
-ls -l .
-cat parmed-*
