aeead84bf6aa5f7efd0698b2aaf16be283fd6c05
nhenriksen <shireham@gmail.com>
2018-01-17 12:36:39 -0800

    fixes parmed installation for Travis

--- devtools/travis/install.sh
+++ devtools/travis/install.sh
@@ -5,12 +5,12 @@ conda config --add channels omnia --add channels conda-forge
 #conda install -y openmm
 
 conda create -y -n myenv python=$PYTHON_VERSION \
-      numpy scipy pandas
+      numpy scipy pandas parmed
 
 #conda install -y -n myenv \
 #      ambertools=17.0 -c http://ambermd.org/downloads/ambertools/conda/
 
-conda install -y parmed
+#conda install -y -n  parmed
 
 pip install codecov
 
