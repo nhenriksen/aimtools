cefde612e737d4a96b0773718575b113ea28a2b4
nhenriksen <shireham@gmail.com>
2018-01-18 21:45:27 -0800

    cleans up frcmod test and adds mol2 test

--- aimtools/tests/test_unique_types.py
+++ aimtools/tests/test_unique_types.py
@@ -5,12 +5,14 @@ import pytest
 
 path = '.'
 
-def test_unique_types():
+def test_unique_types_frcmod():
     parm = pmd.amber.LoadParm(path+'/thf.prmtop')
     uniq_types = assign_uniq_types(parm,path+'/thf.EQUIVATOMS.DAT')
     write_unique_frcmod(parm,uniq_types,path+'/thf.uniq.frcmod')
     assert filecmp.cmp(path+'/REF.thf.uniq.frcmod',path+'/thf.uniq.frcmod')
-
+    
+    write_unique_mol2('thf.mol2',uniq_types,'thf.uniq.mol2')
+    assert filecmp.cmp(path+'/REF.thf.uniq.mol2',path+'/thf.uniq.mol2')
 
 
 
