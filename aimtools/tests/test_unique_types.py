cb8b3f16da60f32b3ace616cde5802204770f812
nhenriksen <shireham@gmail.com>
2018-01-18 20:47:48 -0800

    tests codecov

--- aimtools/tests/test_unique_types.py
+++ aimtools/tests/test_unique_types.py
@@ -3,11 +3,13 @@ from aimtools.unique_types import *
 import filecmp
 import pytest
 
+path = '.'
+
 def test_unique_types():
-    parm = pmd.amber.LoadParm('aimtools/tests/thf.prmtop')
-    uniq_types = assign_uniq_types(parm,'aimtools/tests/thf.EQUIVATOMS.DAT')
-    write_unique_frcmod(parm,uniq_types,'aimtools/tests/thf.uniq.frcmod')
-    assert filecmp.cmp('aimtools/tests/REF.thf.uniq.frcmod','aimtools/tests/thf.uniq.frcmod')
+    parm = pmd.amber.LoadParm(path+'/thf.prmtop')
+    uniq_types = assign_uniq_types(parm,path+'/thf.EQUIVATOMS.DAT')
+    write_unique_frcmod(parm,uniq_types,path+'/thf.uniq.frcmod')
+    assert filecmp.cmp(path+'/REF.thf.uniq.frcmod',path+'/thf.uniq.frcmod')
 
 
 
