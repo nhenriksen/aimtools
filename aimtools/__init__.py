907af74fb957aa9e546e56ba052a4e502a46c08a
nhenriksen <shireham@gmail.com>
2018-01-26 09:15:17 -0800

    Fixes _init__ import for python3

--- aimtools/__init__.py
+++ aimtools/__init__.py
@@ -2,6 +2,6 @@
 The aimtools package provides tools for using the atoms-in-molecules approach with MD force fields in AMBER.
 """
 
-from unique_types import *
+from aimtools.unique_types import *
 
 __all__ = ["unique_types"]
