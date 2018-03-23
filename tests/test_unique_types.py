ad99fc098d673d3a13c13ac05e34f922b62aa1aa
nhenriksen <shireham@gmail.com>
2018-03-23 16:30:44 -0700

    Reconfigures command line; adds support for not using equiv_file; reconfigures tests

--- tests/test_unique_types.py
+++ tests/test_unique_types.py
@@ -2,16 +2,28 @@ import parmed as pmd
 from aimtools.unique_types import *
 import filecmp
 import pytest
+import os
+import shutil
 
 
 def test_unique_types():
+    out_dir = 'thf_uniq_output'
+    if os.path.exists(out_dir):
+        shutil.rmtree(out_dir)
+    os.makedirs(out_dir)
     parm = pmd.amber.LoadParm('thf.prmtop')
     unique_types = create_unique_type_list(parm,'thf.EQUIVATOMS.DAT')
-    write_unique_frcmod_mol2s(parm,unique_types,'thf.uniq.frcmod',name_list='thf.uniq')
-    assert filecmp.cmp('REF.thf.uniq.frcmod','thf.uniq.frcmod')
-    assert filecmp.cmp('REF.thf.uniq.mol2','thf.uniq.mol2')
+    write_unique_frcmod_mol2s(parm,unique_types,'thf.uniq.frcmod',names='thf.uniq',path=out_dir)
+    assert filecmp.cmp('REF.thf.uniq.frcmod',out_dir+'/thf.uniq.frcmod')
+    assert filecmp.cmp('REF.thf.uniq.mol2',out_dir+'/thf.uniq.mol2')
+    shutil.rmtree(out_dir)
 
 def test_batch_unique_types():
+    out_dir = 'batch_uniq_output'
+    if os.path.exists(out_dir):
+        shutil.rmtree(out_dir)
+    os.makedirs(out_dir)
+
     parm_list = []
     equiv_list = []
 
@@ -25,8 +37,9 @@ def test_batch_unique_types():
         equiv_list.append(mol+'.EQUIVATOMS.DAT')
 
     unique_types = create_unique_type_list(parm_list, equiv_list)
-    write_unique_frcmod_mol2s(parm_list, unique_types, 'batch.frcmod')
-    assert filecmp.cmp('REF.batch.frcmod','batch.frcmod')
+    write_unique_frcmod_mol2s(parm_list, unique_types, 'batch.frcmod',path=out_dir)
+    assert filecmp.cmp('REF.batch.frcmod',out_dir+'/batch.frcmod')
+    shutil.rmtree(out_dir)
 
 
 
